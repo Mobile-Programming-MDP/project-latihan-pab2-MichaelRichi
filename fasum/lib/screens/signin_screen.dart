a5aa6fee64ec98c8dd7bd29ed75580f792bb75fc
MichaelRichi <michaelrichi_2226240154@mhs.mdp.ac.id>
2025-04-25 19:14:09 +0700

    Update Path Depedency
    Remove Unimportant Import, Update Path Depedency

--- fasum/lib/screens/signin_screen.dart
+++ fasum/lib/screens/signin_screen.dart
@@ -1,7 +1,6 @@
 import 'package:fasum/screens/home_screen.dart';
 import 'package:fasum/screens/signup_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class SigninScreen extends StatefulWidget {
--- fasum/lib/screens/signup_screen.dart
+++ fasum/lib/screens/signup_screen.dart
@@ -1,6 +1,5 @@
 import 'package:fasum/screens/signin_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class SignupScreen extends StatefulWidget {
